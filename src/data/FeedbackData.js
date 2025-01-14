27dfdc8e0202832ff928206293d3fa16645bfbe3
Alexander <eadsontech@gmail.com>
2025-01-14 12:49:37 +0100

    initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,29 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 3,
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, qui quidem hic quos praesentium modi neque eum laborum perspiciatis? Ea aperiam voluptate dolore. Laboriosam aperiam corporis nisi quo voluptate quam earum officiis reprehenderit iusto sunt fugit laudantium, ipsam facilis pariatur.'
+    },
+    {
+        id: 2,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, qui quidem hic quos praesentium modi neque eum laborum perspiciatis? Ea aperiam voluptate dolore. Laboriosam aperiam corporis nisi quo voluptate quam earum officiis reprehenderit iusto sunt fugit laudantium, ipsam facilis pariatur.'
+    },
+    {
+        id: 3,
+        rating: 5,
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, qui quidem hic quos praesentium modi neque eum laborum perspiciatis? Ea aperiam voluptate dolore. Laboriosam aperiam corporis nisi quo voluptate quam earum officiis reprehenderit iusto sunt fugit laudantium, ipsam facilis pariatur.'
+    },
+    {
+        id: 4,
+        rating: 1,
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, qui quidem hic quos praesentium modi neque eum laborum perspiciatis? Ea aperiam voluptate dolore. Laboriosam aperiam corporis nisi quo voluptate quam earum officiis reprehenderit iusto sunt fugit laudantium, ipsam facilis pariatur.'
+    },
+    {
+        id: 5,
+        rating: 9,
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, qui quidem hic quos praesentium modi neque eum laborum perspiciatis? Ea aperiam voluptate dolore. Laboriosam aperiam corporis nisi quo voluptate quam earum officiis reprehenderit iusto sunt fugit laudantium, ipsam facilis pariatur.'
+    },
+]
+
+export default FeedbackData;
