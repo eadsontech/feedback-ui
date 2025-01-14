9f11f8b8d225bc79edd2b33332967b5ad0cd7e44
Alexander <eadsontech@gmail.com>
2025-01-14 15:51:03 +0100

    added the backend using json server

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,9 @@
+
+import spinner from '../assets/spinner.gif'
+
+function Spinner() {
+  return (
+    <img src={spinner} alt="Loading animation" style={{width: '100px', margin: 'auto', display: 'block'}} />
+  )
+}
+export default Spinner
